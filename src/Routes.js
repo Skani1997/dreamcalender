e95caa14adcf04bf07ca1014b6627205104b7d6e
Skani1997 <63139286+Skani1997@users.noreply.github.com>
2021-07-28 14:57:45 +0900

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { BrowserRouter , Route , Switch} from 'react-router-dom';
+import { BrowserRouter , Route} from 'react-router-dom';
 import SurveyP from './SurveyPage';
 import Home from './Home';
 
 export default () => (
     <div >
       <BrowserRouter>
-        <Switch>
             <Route exact={true} path="" component={Home}/>
             <Route path="/surveypage" component={SurveyP}/>
-        </Switch>
       </BrowserRouter>
     </div>
   )
