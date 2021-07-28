89d452e2da71b6bf40d0630b6ff8ce30217d6183
Skani1997 <shkim9709@gmail.com>
2021-07-28 16:26:55 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dreamcalender/precache-manifest.433618bee9ce33538debd8a4488cb6e5.js"
+  "/dreamcalender/precache-manifest.f771210515b2273db40aba54e85ec552.js"
 );
 
 self.addEventListener('message', (event) => {
