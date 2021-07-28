11682639eb0ff3dde2a823fb792942d14a29ee07
Skani1997 <63139286+Skani1997@users.noreply.github.com>
2021-07-28 11:09:22 +0900

    update survey page routing

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { HashRouter , Route } from 'react-router-dom';
+import { HashRouter , Route , Switch} from 'react-router-dom';
 import SurveyP from './SurveyPage';
 import Home from './Home';
 
 export default () => (
     <div >
       <HashRouter>
-        <Route exact={true} path="" component={Home}/>
-        <Route path="/#/surveypage" component={SurveyP}/>
+        <Switch>
+            <Route exact path="" component={Home}/>
+            <Route path="/#/surveypage" component={SurveyP}/>
+        </Switch>
       </HashRouter>
     </div>
   )
